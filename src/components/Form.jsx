26e6e18a9ad22bfdd547a44bb16a587a042706b9
Denys <mail@mail.ru>
2021-02-20 19:24:19 +0200

    made a bold Form input

---
+++ src/components/Form.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Form() {
+  return (
+   <form>
+     <input type="text" className="form-control" />
+   </form>
+  );
+}
+
+export default Form;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Form from '../components/Form'
 
 
 function Home() {
   return (
     <div className="container">
-      Home
+      <Form />
     </div>
   );
 }
